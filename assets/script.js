841665e83d735d2942158b8f26e12981c55c7e8a
kaiquebonini <ks.bonini@gmail.com>
2024-06-07 01:22:19 -0300

    desligar carroussel automatico

--- assets/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var myCarousel = document.querySelector('#groupCarousel');
-    new bootstrap.Carousel(myCarousel, {
-        interval: 0
-    });
-});
