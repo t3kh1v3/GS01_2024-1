d7425d49f483f108ad236ece04cc7fdf986dbc25
kaiquebonini <ks.bonini@gmail.com>
2024-06-07 01:20:43 -0300

    pasta assets criada e imagens adicionadas

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     var myCarousel = document.querySelector('#groupCarousel');
-    var carousel = new bootstrap.Carousel(myCarousel, {
-        interval: 2000
+    new bootstrap.Carousel(myCarousel, {
+        interval: 0
     });
 });
