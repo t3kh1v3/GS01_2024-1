e10d9c5ef5efc3e13ba5fd368861f35c04fcf140
kaiquebonini <ks.bonini@gmail.com>
2024-06-07 23:25:32 -0300

    fix roleta

--- javascript/script.js
+++ javascript/script.js
@@ -10,26 +10,11 @@ function playOnClick() {
 	
 	var random = Math.floor(Math.random() * 10) + 1
 	setTimeout(() => {
-		console.log('stopping')
 		stopOnClick();
 	}, 1000 * random);
-}
-
-function calculate() {
-	var timeFinal = new Date();
-	var tempo = Math.abs(timeFinal - globalObjects.timeInitial);
-	var box = parseInt(tempo / 250);
-	if (box > 7)
-			box = parseInt(box % 8);
-	
-	console.log(globalObjects.timeInitial, timeFinal, tempo, box, (tempo / 250));
-	return box;
 }
 
 function stopOnClick() {
 	globalObjects.roleta.style["animation-play-state"] = "paused";
 	globalObjects.btnPlay.style.display = "block";
-	var box = calculate();
-	var boxGanhador = document.getElementById("opt".concat(box))
-	document.getElementById("msgGanhador").innerHTML = "Parabéns! Você ganhou ".concat(boxGanhador.innerHTML);
 }
